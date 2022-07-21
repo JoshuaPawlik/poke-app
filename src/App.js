0ecfe5ad9fa7decd2aae144d1befe4b109b09605
Joshua Pawlik <joshuapawlik12@gmail.com>
2022-07-21 13:11:09 -0500

    Display Pokemon names / Set up State

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useState, useEffect} from 'react'
+import PokemonList from './PokemonList';
+import axios from 'axios';
+
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setcurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel= c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+  }, [currentPageUrl])
+
+  if (loading) return "Loading..."
+
   return (
-    null
+    <PokemonList pokemon={pokemon}/>
   );
 }
 
